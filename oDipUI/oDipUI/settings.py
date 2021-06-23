e1eebf165d6eda4fb583eaf24c049f2bb82bbda9
Christopher Holmes <christopher.holmes@newcastle.co.uk>
2021-06-23 01:52:33 +0100

    removed environ code

--- oDipUI/oDipUI/settings.py
+++ oDipUI/oDipUI/settings.py
@@ -10,13 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-import environ
 from pathlib import Path
 import os
-
-# Setting Environment Variables
-env = environ.Env()
-environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))

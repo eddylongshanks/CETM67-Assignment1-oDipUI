3e95c21cba9f00152c5d72afdc10abf55707984d
Christopher Holmes <christopher.holmes@newcastle.co.uk>
2021-06-23 03:26:52 +0100

    updated to modern syntax

--- oDipUI/oDipUI/settings.py
+++ oDipUI/oDipUI/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (
-    os.path.join(SITE_ROOT, '..', 'static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 # Default primary key field type

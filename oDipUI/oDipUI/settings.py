e25a2ee852f7c9267544c3b3d37ca9580fe9d6d2
Christopher Holmes <christopher.holmes@newcastle.co.uk>
2021-06-23 22:13:27 +0100

    static tweak

--- oDipUI/oDipUI/settings.py
+++ oDipUI/oDipUI/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'oDipUI.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, '/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,11 +120,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
 
 
 # Default primary key field type

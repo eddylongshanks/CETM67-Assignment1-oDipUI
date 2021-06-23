626615a1b615a25ee9c0eda088a26413be954dd9
Christopher Holmes <christopher.holmes@newcastle.co.uk>
2021-06-23 03:50:15 +0100

    static tweaking

--- oDipUI/oDipUI/settings.py
+++ oDipUI/oDipUI/settings.py
@@ -120,8 +120,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT = ''
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 
 # Default primary key field type
